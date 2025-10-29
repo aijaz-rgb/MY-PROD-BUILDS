63de27a16f389a652bbc4e8cb75accf2e738555e
aijaz-rgb <aijazs@outlook.com>
2025-10-29 12:15:12 +0400

    Update backend.tf

--- azure-terraform-builds-main/environments/dev/backend.tf
+++ azure-terraform-builds-main/environments/dev/backend.tf
@@ -5,9 +5,9 @@ terraform {
 
   backend "azurerm" {
     # These values must be consistent across all environments
-    resource_group_name  = "New_App_RG1-TERRA"  
-    storage_account_name = "newst1terra1982" 
-    container_name       = "containerterra"            
+    resource_group_name  = "gen-ai-rg"  
+    storage_account_name = "genaistorageterra" 
+    container_name       = "statefilecontainer"            
     
     # This value must be UNIQUE for this 'dev' environment
     key                  = "environments/dev/terraform.tfstate" 
